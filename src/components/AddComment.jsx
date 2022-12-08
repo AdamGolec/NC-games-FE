212d08b36e7bd0cf7e3645d556a703c70024a0e8
Decyl0 <adamgolec93@gmail.com>
2022-12-08 17:10:46 +0000

    add AddComment file

---
+++ src/components/AddComment.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { postComment } from "../utils/api";
+import { useParams } from "react-router-dom";
+
+const AddComment = (props) => {
+  const [newComment, setNewComment] = useState("");
+  const [username, setUsername] = useState("");
+  const [postStatus, setPostStatus] = useState("");
+  const [showStatus, setShowStatus] = useState(true);
+  const { review_id } = useParams();
+  const {setComments} = props
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    postComment(newComment, username, review_id)
+      .then((commentData) => {
+        setNewComment((currComments) => {
+          const newComments = [...currComments];
+          newComments.push(commentData);
+          return newComments;
+          
+        });
+        setPostStatus("Thanks for posting your comment!");
+        setShowStatus(false);
+      })
+      .catch((error) => {
+        setPostStatus(
+          "Something went wrong :( Make sure you're using existing Username"
+        );
+      });
+      
+    };
+
+  return (
+    <>
+      {showStatus && (
+        <>
+          <form className="add-comment__form" onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input
+              id="username"
+              type="text"
+              value={username}
+              placeholder="Existing username"
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            ></input>
+            <label htmlFor="newComment">Add comment</label>
+            <textarea
+              placeholder="Your comment here..."
+              id="newComment"
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+              required
+            ></textarea>
+
+            <button>Post</button>
+          </form>
+        </>
+      )}
+      <p>{postStatus}</p>
+    </>
+  );
+};
+
+export default AddComment;
